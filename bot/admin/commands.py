dd17b714d41e8eaf2d7d23e92d3689e7f61ffb13
Anasxon Ummataliyev <aummataliy@gmail.com>
2025-12-10 17:29:49 +0500

    fix

--- bot/admin/commands.py
+++ bot/admin/commands.py
@@ -2,5 +2,7 @@ from aiogram.types import BotCommand
 
 
 commands = [
-    BotCommand(command='/start', description='Botni boshlash')
+    BotCommand(command='/start', description='Botni boshlash'),
+    BotCommand(command='/help', description='Yordam'),
+    BotCommand(command='/reply', description='userga xabar yuborish'),
 ]
