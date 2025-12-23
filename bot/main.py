4ad770b88e82d15fb47390b6ba70ceeec15a7495
Anasxon Ummataliyev <aummataliy@gmail.com>
2025-12-23 22:25:15 +0500

    fix

--- bot/main.py
+++ bot/main.py
@@ -7,11 +7,15 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message, BotCommandScopeChat
+from bot.middlewares.activity import UserActivityMiddleware
+from bot.middlewares.channel import IsJoinChannelMiddleware
 
-from bot.admin.start import admin_commands 
+from bot.admin.start import admin_commands
 from bot.admin import admin_router
 
+
 from dotenv import load_dotenv
+
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
@@ -19,20 +23,27 @@ ADMIN = int(os.getenv("ADMIN"))
 bot = Bot(TOKEN)
 dp = Dispatcher()
 
+
 @dp.startup()
 async def startup(bot: Bot):
-    await bot.set_my_commands(commands=admin_commands, scope=BotCommandScopeChat(chat_id=ADMIN))
-    await bot.send_message(chat_id=ADMIN, text='Bot started✅')
+    await bot.set_my_commands(
+        commands=admin_commands, scope=BotCommandScopeChat(chat_id=ADMIN)
+    )
+    await bot.send_message(chat_id=ADMIN, text="Bot started✅")
+
 
 @dp.shutdown()
 async def shutdown(bot: Bot):
-    await bot.send_message(chat_id=ADMIN, text='Bot stopped❌')
+    await bot.send_message(chat_id=ADMIN, text="Bot stopped❌")
 
 
 async def main():
+    dp.message.middleware(IsJoinChannelMiddleware)
+    dp.message.middleware(UserActivityMiddleware)
     dp.include_router(admin_router)
     await dp.start_polling(bot)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
