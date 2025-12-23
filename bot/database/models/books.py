2ad1fc37a867b2a612f32f10aad31ff86e48d350
Anasxon Ummataliyev <aummataliy@gmail.com>
2025-12-23 22:23:17 +0500

    added user, book, and channels

---
+++ bot/database/models/books.py
@@ -0,0 +1,16 @@
+from typing import Optional
+from pydantic import BaseModel
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+class Books(BaseModel):
+    __tablename__ = "books"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(String(300), nullable=False, index=True)
+    author: Mapped[str] = mapped_column(String(200), nullable=False, index=True)
+    cover_image_id: Optional[str] = None
+    file_id: Mapped[str] = mapped_column(String(200), nullable=False, unique=True)
+    file_size: Mapped[Optional[int]] = mapped_column(Integer, nullable=True)
+    language: Mapped[str] = mapped_column(String(10), default="en")
+    description: Mapped[Optional[str]] = mapped_column(String(1000), nullable=True)
