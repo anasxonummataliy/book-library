d5a9bef2509feeb07f2989060583a4a924da801b
Anasxon Ummataliyev <aummataliy@gmail.com>
2026-02-24 10:58:29 +0500

    fix

--- bot/database/models/books.py
+++ bot/database/models/books.py
@@ -1,7 +1,9 @@
 from typing import Optional
-from pydantic import BaseModel
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
+
+from bot.database.base import BaseModel
+
 
 class Book(BaseModel):
     __tablename__ = "books"
@@ -9,7 +11,7 @@ class Book(BaseModel):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     title: Mapped[str] = mapped_column(String(300), nullable=False, index=True)
     author: Mapped[str] = mapped_column(String(200), nullable=False, index=True)
-    cover_image_id: Optional[str] = None
+    cover_image_id: Mapped[Optional[str]] = mapped_column(String, nullable=True)
     file_id: Mapped[str] = mapped_column(String(200), nullable=False, unique=True)
     file_size: Mapped[Optional[int]] = mapped_column(Integer, nullable=True)
     language: Mapped[str] = mapped_column(String(10), default="en")
