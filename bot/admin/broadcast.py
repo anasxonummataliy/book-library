5b1319194e75dc547f91a1c1e3037c796bf480dd
Anasxon Ummataliyev <aummataliy@gmail.com>
2026-02-24 10:55:40 +0500

    fix

--- bot/admin/broadcast.py
+++ bot/admin/broadcast.py
@@ -1,9 +1,19 @@
+import asyncio
 from aiogram import Router
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, User
 
 router = Router()
 
+
 @router.message(Command("broadcast"))
 async def broadcast_handler(message: Message):
-    pass
+    users = await User.get_all()
+    for user in users:
+        try:
+            await message.bot.send_message(
+                user.tg_id, "Bu xabar barcha foydalanuvchilarga yuborilmoqda!"
+            )
+            await asyncio.sleep(0.1)
+        except Exception as e:
+            print(f"Xatolik yuz berdi: {e}")
