af965a465221aa6d55764d877225b1dbd0b7cec9
Anasxon Ummataliyev <aummataliy@gmail.com>
2026-02-25 11:38:18 +0500

    fix

--- bot/routers/__init__.py
+++ bot/routers/__init__.py
@@ -1,7 +1,10 @@
 from aiogram import Router
 
+from bot.filters import isAdmin
+
 from .basic import main_router
 from .message_to_admin import message_to_admin_router
 
 user_router = Router()
+user_router.message.filter(~isAdmin())
 user_router.include_routers(main_router, message_to_admin_router)
